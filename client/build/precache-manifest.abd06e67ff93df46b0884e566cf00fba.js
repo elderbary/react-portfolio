027cb2c256e61694724b2b2b0bdb35e5d7f102a4
Dawid Rozanski <drozanski@outlook.com>
2020-05-22 18:00:22 +0200

    sd

---
+++ client/build/precache-manifest.abd06e67ff93df46b0884e566cf00fba.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a903294d4aa489779b30c8d266113f86",
+    "url": "/elderbary/react-portfolio.git/index.html"
+  },
+  {
+    "revision": "3b3585696df90074b131",
+    "url": "/elderbary/react-portfolio.git/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "c77e7d4007e5f66442d9",
+    "url": "/elderbary/react-portfolio.git/static/css/main.f168d340.chunk.css"
+  },
+  {
+    "revision": "3b3585696df90074b131",
+    "url": "/elderbary/react-portfolio.git/static/js/2.e7f20435.chunk.js"
+  },
+  {
+    "revision": "630387564c61dbcc034ad15a39c58221",
+    "url": "/elderbary/react-portfolio.git/static/js/2.e7f20435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77e7d4007e5f66442d9",
+    "url": "/elderbary/react-portfolio.git/static/js/main.c2c5056e.chunk.js"
+  },
+  {
+    "revision": "b5c040d4383853766d7e",
+    "url": "/elderbary/react-portfolio.git/static/js/runtime-main.9fe1101b.js"
+  },
+  {
+    "revision": "e4fdf3a74239d4645ba1d2c3b970acf2",
+    "url": "/elderbary/react-portfolio.git/static/media/aleksandra-wolna.e4fdf3a7.jpg"
+  },
+  {
+    "revision": "bbb62dfd95ad00eb625f412f7acb5282",
+    "url": "/elderbary/react-portfolio.git/static/media/czlowiek-spoiler.bbb62dfd.png"
+  },
+  {
+    "revision": "75eb8f03cbe3bfb32d2f1a6ae266bb9c",
+    "url": "/elderbary/react-portfolio.git/static/media/facebook.75eb8f03.svg"
+  },
+  {
+    "revision": "ffceac7b87a77d0b7b23c5b36b922cbd",
+    "url": "/elderbary/react-portfolio.git/static/media/instagram.ffceac7b.svg"
+  },
+  {
+    "revision": "b52f89f84ea08559b5b2e2988d9d6994",
+    "url": "/elderbary/react-portfolio.git/static/media/polacy-nie-czytaja.b52f89f8.png"
+  },
+  {
+    "revision": "5cd903ce754206d029bba7d74be3c6bc",
+    "url": "/elderbary/react-portfolio.git/static/media/recenzja-sucession.5cd903ce.png"
+  },
+  {
+    "revision": "aad2c8afe627d7a78d90d589b10d24a5",
+    "url": "/elderbary/react-portfolio.git/static/media/shitshow.aad2c8af.png"
+  },
+  {
+    "revision": "c2836667908c521f5bbac80f2701f258",
+    "url": "/elderbary/react-portfolio.git/static/media/szczygiel.c2836667.png"
+  },
+  {
+    "revision": "7d5522f722c38aa6bd148e2acff454f0",
+    "url": "/elderbary/react-portfolio.git/static/media/the-morning-show.7d5522f7.png"
+  },
+  {
+    "revision": "297d239db9d8cf013e598ba59719c2aa",
+    "url": "/elderbary/react-portfolio.git/static/media/timeline.297d239d.svg"
+  },
+  {
+    "revision": "bd08eed18de79b787f52204799771a6a",
+    "url": "/elderbary/react-portfolio.git/static/media/twitter.bd08eed1.svg"
+  }
+]);
